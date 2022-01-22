734fbd5f1759e6887e48b8815815d632dc0f0bb7
juliaxcr <juliacrooijmans@gmail.com>
2022-01-22 15:54:51 -0500

    styling navbar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -26,7 +26,7 @@ export const NavbarContainer = styled.div`
   z-index: 1;
   width: 100%;
   padding: 0 24px;
-  max-width: 1100px;
+  max-width: 1700px;
 `;
 
 export const NavLogo = styled(LinkR)`
@@ -59,7 +59,6 @@ export const MobileIcon = styled.div`
 export const NavMenu = styled.ul`
   display: flex;
   align-items: center;
-  justify-item: right;
   list-style: none;
   text-align: center;
   margin-right: -22px;
