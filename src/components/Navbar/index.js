0f112587c9a016344fc4f25f482470f5bfbf7701
juliaxcr <juliacrooijmans@gmail.com>
2022-01-22 11:12:49 -0500

    created toggle for sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,34 +1,42 @@
-import React from 'react'
-import {FaBars} from 'react-icons/fa'
-import {Nav, NavbarContainer, NavItem, NavLinks, NavLogo, NavMenu, MobileIcon} from './NavbarElements';
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavItem,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+  MobileIcon,
+} from "./NavbarElements";
 
-const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to='/'>julia</NavLogo>
-                    <MobileIcon>
-                        <FaBars />
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks to='about'>About</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to='projects'>Projects</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to='photography'>Photography</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to='contact'>Contact</NavLinks>
-                        </NavItem>
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav>
-        </>
-    )
-}
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">julia</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="projects">Projects</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="photography">Photography</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Contact</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import {SidebarContainer, Icon, CloseIcon} from './SidebarElements'
+import React from "react";
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarWrapper,
+} from "./SidebarElements";
 
-const Sidebar = () => {
-    return (
-        <SidebarContainer>
-            <Icon>
-                <CloseIcon />
-            </Icon>
-            <SidebarWrapper>
-            <SidebarMenu>
-                <SidebarLink to='about'>
-                About
-                </SidebarLink>
-                <SidebarLink to='projects'>
-                Projects 
-                </SidebarLink>
-                <SidebarLink to='photography'>
-                Photography
-                </SidebarLink>
-                <SidebarLink to='contact'>
-                Contact
-                </SidebarLink>
-                </SidebarMenu>
-            </SidebarWrapper>
-        </SidebarContainer>
-    )
-}
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about" onClick={toggle}>
+            About
+          </SidebarLink>
+          <SidebarLink to="projects" onClick={toggle}>
+            Projects
+          </SidebarLink>
+          <SidebarLink to="photography" onClick={toggle}>
+            Photography
+          </SidebarLink>
+          <SidebarLink to="contact" onClick={toggle}>
+            Contact
+          </SidebarLink>
+        </SidebarMenu>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
